abe543b68743e5a75411a7543d16eb8052e5ef5f
Vanessa Würdinger <vanessa.wuerdinger@gmx.de>
2022-10-03 20:55:09 +0200

    several changes

---
+++ js/loadImages.js
@@ -0,0 +1,27 @@
+let characterImagesWalking = [
+    'img/2_character_pepe/2_walk/W-21.png',
+    'img/2_character_pepe/2_walk/W-22.png',
+    'img/2_character_pepe/2_walk/W-23.png',
+    'img/2_character_pepe/2_walk/W-24.png',
+    'img/2_character_pepe/2_walk/W-25.png',
+    'img/2_character_pepe/2_walk/W-26.png',
+];
+
+let characterImagesCache = [];
+
+
+
+function loadImages() {
+    characterImagesWalking = [
+        'img/2_character_pepe/2_walk/W-21.png',
+        'img/2_character_pepe/2_walk/W-22.png',
+        'img/2_character_pepe/2_walk/W-23.png',
+        'img/2_character_pepe/2_walk/W-24.png',
+        'img/2_character_pepe/2_walk/W-25.png',
+        'img/2_character_pepe/2_walk/W-26.png',
+    ].forEach((path) => {
+        let img = new Image();
+        img.src = path;
+        characterImagesCache.push(characterImagesCache[path] = img);
+    })
+}

99f35d99bcfddf1d1adc5ff62a51b23032ac1f44
Vanessa Würdinger <vanessa.wuerdinger@gmx.de>
2022-10-11 18:17:14 +0200

    different changes

--- models/character.class.js
+++ models/character.class.js
@@ -10,11 +10,6 @@ class Character extends MovableObject {
         'left': 30,
     }
     world;
-    walking_sound = walking_sound;
-    jumping_sound = jumping_sound;
-    hurting_sound = hurting_sound;
-    dying_sound = dying_sound;
-    loose_sound = loose_sound;
 
     constructor() {
         super()
@@ -30,6 +25,7 @@ class Character extends MovableObject {
             setInterval(() => {
                 if (this.position_x >= 700) {
                     isEndbossReached = true;
+                    this.img = characterImagesCache['img/2_character_pepe/1_idle/idle/I-1.png']
                     game_music.pause();
                     //interval should stop if endboss is reached
                     clearInterval(endbossAnimationInterval);
@@ -39,68 +35,94 @@ class Character extends MovableObject {
     }
 
     animate() {
+        this.runSkills();
+        this.runAnimation();
+    }
+
+    runSkills() {
         setInterval(() => {
             if (!pauseGame) {
+                // character can not move when endboss is showing up
                 if (!isEndbossReached) {
-                    this.walking_sound.pause();
-                    if (this.world.keyboard.RIGHT && this.position_x < this.world.level.level_end_x) {
-                        this.moveRight();
-                        this.otherDirection = false;
-                        this.walking_sound.play();
-                    }
-
-                    if (this.world.keyboard.LEFT && this.position_x > 0) {
-                        this.moveLeft();
-                        this.otherDirection = true;
-                        this.walking_sound.play();
-                    }
-
-                    if (this.world.keyboard.SPACE && !this.isAboveGround()) {
-                        this.jump();
-                        this.jumping_sound.play();
-                    }
-
+                    walking_sound.pause();
+                    this.checkDirectionRight();
+                    this.checkDirectionLeft();
+                    this.checkJumping();
+                    //adjust camera
                     this.world.camera_x = -this.position_x + 100;
                 }
             }
-        }, 1000 / 60);
+        }, 1000 / 60)
+    }
 
-        // Different images for dead, hurt, juming, walking 
+    checkDirectionRight() {
+        if (this.world.keyboard.RIGHT && this.position_x < this.world.level.level_end_x) {
+            this.moveRight();
+            this.otherDirection = false;
+            walking_sound.play();
+        }
+    }
 
+    checkDirectionLeft() {
+        if (this.world.keyboard.LEFT && this.position_x > 0) {
+            this.moveLeft();
+            this.otherDirection = true;
+            walking_sound.play();
+        }
+    }
+
+    checkJumping() {
+        if (this.world.keyboard.SPACE && !this.isAboveGround()) {
+            this.jump();
+            jumping_sound.play();
+        }
+    }
+
+
+    runAnimation() {
         setInterval(() => {
             if (!pauseGame) {
-                if (this.isDead()) {
-                    this.playAnimation(characterImagesDead)
-                    this.dying_sound.play();
-                    this.looseGame();
-                } else if (this.isHurt()) {
-                    this.hurting_sound.play();
-                    this.playAnimation(characterImagesHurt);
-                } else if (this.characterIsMoving() && !this.isAboveGround()) {
-                    // walk animation 
-                    this.playAnimation(characterImagesWalking)
+                if (!isEndbossReached) {
+                    if (this.isDead()) {
+                        this.playAnimation(characterImagesDead)
+                        dying_sound.play();
+                        this.looseGame();
+                    } else if (this.isHurt()) {
+                        hurting_sound.play();
+                        this.playAnimation(characterImagesHurt);
+                    } else if (this.characterIsMoving() && !this.isAboveGround()) {
+                        // walk animation 
+                        this.playAnimation(characterImagesWalking)
+                    }
                 }
             }
         }, 50);
 
         setInterval(() => {
             if (!pauseGame) {
-                if (this.isAboveGround()) {
-                    this.playAnimation(characterImagesJumping)
+                if (!isEndbossReached) {
+                    if (this.isAboveGround()) {
+                        this.playAnimation(characterImagesJumping)
+                    }
                 }
             }
         }, 180);
 
         setInterval(() => {
             if (!pauseGame) {
-                if (this.noKeyboardButtonSelected() && !this.isAboveGround()) {
-                    this.playAnimation(characterImagesIdle)
+                if (!isEndbossReached) {
+                    if (this.noKeyboardButtonSelected() && !this.isAboveGround()) {
+                        this.playAnimation(characterImagesIdle)
+                    }
                 }
             }
         }, 130)
     }
 
-    characterIsMoving(){
+    // Different images for dead, hurt, juming, walking 
+
+
+    characterIsMoving() {
         return (this.world.keyboard.RIGHT || this.world.keyboard.LEFT)
     }
 
@@ -114,7 +136,7 @@ class Character extends MovableObject {
 
     looseGame() {
         setTimeout(() => {
-            this.loose_sound.play();
+            loose_sound.play();
             game_music.volume = false;
             game_music.src = './audio/music.mp3';
             this.img = characterImagesCache['img/2_character_pepe/5_dead/D-56.png'];
@@ -127,5 +149,37 @@ class Character extends MovableObject {
             game_music.volume = false;
             clearAllIntervals();
         }, 500)
+    }
+    
+    playJumpAnimation(images) {
+        // console.log(this.speedY)   
+        if (this.speedY <= 30 && this.speedY > 20) {
+            this.jumpingImage(images[0])
+        }
+        else if (this.speedY <= 20 && this.speedY > 10) {
+            this.jumpingImage(images[1])
+        }
+        else if (this.speedY <= 10 && this.speedY > 0) {
+            this.jumpingImage(images[2])
+        }
+        else if (this.speedY <= 0 && this.speedY > -7.5) {
+            this.jumpingImage(images[3])
+        }
+        else if (this.speedY <= -7.5 && this.speedY > -15) {
+            this.jumpingImage(images[4])
+        }
+        else if (this.speedY <= -15 && this.speedY > -22.5) {
+            this.jumpingImage(images[5])
+        }
+        else if (this.speedY <= -22.5 && this.speedY > -30) {
+            this.jumpingImage(images[6])
+        }
+        else if (this.speedY < -30) {
+            this.jumpingImage(images[7])
+        }
+    };
+
+    jumpingImage(image) {
+        this.img = this.imageCache[image];
     }
 }  
